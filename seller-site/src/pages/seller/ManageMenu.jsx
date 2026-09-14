f6b7ea26f6e519fec9b4b0df97bb689c115926da
Att <m.attayaardhana02@gmail.com>
2026-09-14 15:26:20 +0700

    Frontend seller Stg 6: Added contents in manageMenu

--- seller-site/src/pages/seller/ManageMenu.jsx
+++ seller-site/src/pages/seller/ManageMenu.jsx
@@ -1,5 +1,44 @@
+import {useState, useEffect} from 'react';
+import api from '../../utils/api';
+
 function ManageMenu() {
-    return <h1>Manage Menu</h1>;
+    const [menuItems, setMenuItems] = useState([])
+    const [standId, setStandId] = useState(null)
+    const [loading, setLoading ]= useState(true)
+
+    async function fetchMenu(){
+        try {
+            const assignmentRes = await api.get('/seller/my-assignment');
+            const mystandId = assignmentRes.data.assignment.stand_id;
+
+            const menuRes = await api.get(`/menu/${mystandId}`);
+            setMenuItems(menuRes.data.stand);
+            setStandId(mystandId);
+        } catch (err) {
+            console.error(err);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(()=>{
+        fetchMenu();
+    }, [])
+
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+    return (
+        <div>
+            <h1>Manage Menu</h1>
+            {menuItems.length === 0 && <p>No menu items yet.</p>}
+            {menuItems.map((item) => (
+                <div key={item.id}>
+                    <p>{item.name} — Rp{item.price} — Qty: {item.quantity}</p>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default ManageMenu;
