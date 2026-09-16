342edd092bce147c29b6f00aa1d92c61fc91a25d
Att <m.attayaardhana02@gmail.com>
2026-09-16 09:48:59 +0700

    Stg 17c: Fixed the payment controller again

--- backend/src/routes/payment.routes.js
+++ backend/src/routes/payment.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authenticate = require('../middleware/authenticate');
 const upload = require('../middleware/upload');
-const { uploadPaymentProofController, reviewPaymentProofController } = require('../controllers/payment.controller');
+const { uploadPaymentProofController, reviewPaymentProofController, getPaymentProofByOrderController } = require('../controllers/payment.controller');
 
 router.post('/upload',authenticate, upload.single('paymentProof'), uploadPaymentProofController);
 router.patch('/:id/review', authenticate, reviewPaymentProofController);
