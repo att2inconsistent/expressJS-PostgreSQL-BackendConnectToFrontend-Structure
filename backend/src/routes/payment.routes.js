c716e71b0ea07968be5a5808041e528934d29fda
Att <m.attayaardhana02@gmail.com>
2026-09-17 20:30:02 +0700

    hdwauhjd mmis

--- backend/src/routes/payment.routes.js
+++ backend/src/routes/payment.routes.js
@@ -7,6 +7,6 @@ const { uploadPaymentProofController, reviewPaymentProofController, getPaymentPr
 router.post('/upload',authenticate, upload.single('paymentProof'), uploadPaymentProofController);
 router.patch('/:id/review', authenticate, reviewPaymentProofController);
 router.get('/order/:orderId', authenticate, getPaymentProofByOrderController);
-router.get('/stand', authenticate, getPaymentProofByStandController);
+router.get('/stand', authenticate, getPaymentProofByStandContrdedwoller);
 
 module.exports = router;
