9a39a0f504dcda428b7b549297b090c06fd6aafe
Att <m.attayaardhana02@gmail.com>
2026-09-23 07:15:20 +0700

    Frontend seller Stg 8: Updated seller appl

--- seller-site/src/pages/admin/SellerApplications.jsx
+++ seller-site/src/pages/admin/SellerApplications.jsx
@@ -1,5 +1,58 @@
+import {useState, useEffect} from 'react';
+import api from '../../utils/api';
+
+const STATUS_OPTIONS = ['pending', 'accepted','rejected'];
+
 function SellerApplications() {
-    return <h1>Seller Applications</h1>;
+    const [applications, setApplications] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    async function fetchOrders() {
+        try {
+            const response = await api.get('/admin/seller-applications');
+            setApplications(response.data.application);
+        } catch (err) {
+            console.error(err);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchOrders();
+    }, []);
+
+    async function handleStatusChange(applicationId, newStatus) {
+        try{
+            await api.patch(`/admin/seller-applications/${applicationId}/status`, { status: newStatus });
+            fetchOrders();
+        }catch(err){
+            console.error(err);
+        }
+    }
+
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+
+    return(
+        <div>
+            <h1>Incoming Applications</h1>
+            {applications.length === 0 && <p>No orders yet.</p>}
+            {applications.map((application) => (
+                <div key={application.id}>
+                    <p>Order #{application.id} — {application.status}</p>
+                    <select value={application.status} onChange={(e) => handleStatusChange(application.id, e.target.value)}>
+                        {STATUS_OPTIONS.map((status) => (
+                            <option key={status} value={status}>
+                                {status}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+            ))}
+        </div>
+    )
 }
 
 export default SellerApplications;
